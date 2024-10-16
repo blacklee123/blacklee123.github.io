ef03e0bc61f9c94037f1ffe9474e3042a3645ff3
李白 <15656564262@163.com>
2024-10-16 18:08:57 +0800

    feat: enable hash route

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -6,7 +6,9 @@ const config: Config = {
   title: '李白',
   tagline: '天下第二也挺好🙂',
   favicon: 'img/favicon.png',
-
+  future: {
+    experimental_router: 'hash',
+  },
   // Set the production url of your site here
   url: 'https://blacklee123.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
@@ -53,88 +55,88 @@ const config: Config = {
     ],
   ],
 
-  themeConfig:{
-      // Replace with your project's social card
-      image: 'img/logo.png',
-      navbar: {
-        title: '李白',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.png',
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/logo.png',
+    navbar: {
+      title: '李白',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.png',
+      },
+      items: [
+        {
+          to: '/docs/notes',
+          label: '笔记',
+          position: 'left',
         },
-        items: [
-          {
-            to: '/docs/notes',
-            label: '笔记',
-            position: 'left',
-          },
-          {
-            to: '/blog',
-            label: '博客',
-            position: 'left',
-          },
-          {
-            to: '/about',
-            label: '关于我',
-            position: 'left',
-          },
-          {
-            href: 'https://github.com/blacklee123/blacklee123.github.io',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/notes/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/blacklee123',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: prismThemes.github,
-        darkTheme: prismThemes.dracula,
-      },
-    } satisfies Preset.ThemeConfig,
+        {
+          to: '/blog',
+          label: '博客',
+          position: 'left',
+        },
+        {
+          to: '/about',
+          label: '关于我',
+          position: 'left',
+        },
+        {
+          href: 'https://github.com/blacklee123/blacklee123.github.io',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Tutorial',
+              to: '/docs/notes/intro',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/blacklee123',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
 }
 
 export default config
